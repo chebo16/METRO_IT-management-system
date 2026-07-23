50c055a37e6618b2db467491e36fee559d91290a
Andrei Cebotari <cebotariandrei16@gmail.com>
2026-07-23 12:14:21 +0300

    application: add initial Java entry point

---
+++ application/src/main/java/com/chebo16/metroit/Main.java
@@ -0,0 +1,9 @@
+package com.chebo16.metroit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("METRO IT Management System");
+        System.out.println("Java 21 and Maven are configured correctly.");
+    }
+}
